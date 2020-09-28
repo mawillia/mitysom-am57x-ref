6393154034fb2662902a01619e7bc968a34a5ace
Michael Williamson <michael.williamson@criticallink.com>
2020-09-28 09:30:10 -0400

    example 1. scripted build - don't apply impl constraints until running impl steps

--- fpga/devkit_basic_gpio/script/build_devkit_bitstream_batch.tcl
+++ fpga/devkit_basic_gpio/script/build_devkit_bitstream_batch.tcl
@@ -14,7 +14,8 @@ read_verilog [ glob ./src/hdl/*.v ]
 read_vhdl [ glob ../ip/common/*.vhd ]
 read_vhdl [ glob ../ip/gpio/*.vhd ]
 read_vhdl [ glob ../ip/gpmc/*.vhd ]
-read_xdc [ glob ./src/constraints/*.xdc ]
+read_xdc ./src/constraints/xilinx_pcie_7x_ep_x2g2.xdc
+read_xdc ./src/constraints/devkit_top.xdc
 #
 #
 read_ip ./ip/clk_wiz_0/clk_wiz_0.xci
@@ -41,6 +42,7 @@ report_power -file $outputDir/post_synth_power.rpt
 #
 #
 # run placement and logic optimization
+read_xdc ./src/constraints/devkit_top_impl.xdc
 opt_design
 place_design
 phys_opt_design
@@ -59,11 +61,12 @@ report_power -file $outputDir/post_route_power.rpt
 report_drc -file $outputDir/post_imp_drc.rpt
 #write_verilog -force $outputDir/bft_impl_netlist.v
 #write_xdc -no_fixed_only -force $outputDir/bft_impl.xdc
+write_xdc $outputDir/post_route_impl.xdc -mode port -force
 #
 #
 # Generate bitstream and binary file
 write_bitstream -force -bin_file $outputDir/devkit.bit
 # Generate file for use with uBoot
 # TODO unclear if -disablebitswap is required
-write_cfgmem -force -format BIN -size 4 -interface SMAPx16 -loadbit "up 0 $outputDir/devkit.bit" -verbose $outputDir/devkit_fpga_uboot.bin
+write_cfgmem -force -format BIN -size 4 -interface SMAPx8 -loadbit "up 0 $outputDir/devkit.bit" -verbose $outputDir/devkit_fpga_uboot.bin
 
